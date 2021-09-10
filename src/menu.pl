ad799528a57eab56cc083b4abed17c14da9e96c0
joaovaladao <joaovaladao161274@gmail.com>
2021-09-10 15:31:43 -0300

    Criação dos cargos e implementação no menu
    Co-authored-by: italofernandes13 <italoserra99@gmail.com>

--- src/menu.pl
+++ src/menu.pl
@@ -1,14 +1,27 @@
-menu :- write(' *** Selecione uma opcao ***'), nl, nl,
+menu :- consult(cargos), nl, nl,
+        write(' *** Selecione uma opcao ***'), nl, nl,
         write('1 - Listar hierarquia de cargos'), nl,
         write('2 - Listar funcionarios'), nl,
         write('3 - Promover funcionario'), nl,
-        write('0 - Sair'), read(Option),
+        write('0 - Sair'), nl,
+        write(''),read(Option),
         nl, executar(Option).
 
-executar(Option) :- Option == 1, hierarquia, menu;
+executar(Option) :- Option == 1, getDescricaoCursos, menu;
                     Option == 2, funcionario, menu;
                     Option == 3, promover, menu;
                     Option == 0, true.
+
+getDescricaoCursos() :-
+  listaCargos(PB),
+  getDescListaCargos(PB).
+
+getDescListaCargos([]).
+getDescListaCargos([X|Y]) :-
+  cargo(X, R),
+  atomic_list_concat([X, R], ' - ', RD), nl,
+  write(RD),
+  getDescListaCargos(Y).
 
 hierarquia :- write('Listando cargos...'), nl, nl.
 
